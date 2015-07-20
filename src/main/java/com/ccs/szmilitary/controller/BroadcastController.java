bc41ab57417f7c96d116aeb85b66f41d493011fd
zhangbin <852774731>
2015-07-20 17:54:38 +0800

    update db

--- src/main/java/com/ccs/szmilitary/controller/BroadcastController.java
+++ src/main/java/com/ccs/szmilitary/controller/BroadcastController.java
@@ -8,7 +8,6 @@ import com.ccs.szmilitary.service.BroadcastService;
 import com.ccs.szmilitary.service.WeaponService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -10,10 +10,43 @@ Target Server Type    : MYSQL
 Target Server Version : 50617
 File Encoding         : 65001
 
-Date: 2015-07-09 16:41:04
+Date: 2015-07-20 17:50:34
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `broadcastlist`
+-- ----------------------------
+DROP TABLE IF EXISTS `broadcastlist`;
+CREATE TABLE `broadcastlist` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `broadcast_name` varchar(255) DEFAULT NULL COMMENT '串联单名称',
+  `broadcast_ctime` varchar(255) DEFAULT NULL COMMENT '串联单创建时间',
+  `weapon_order` int(11) DEFAULT NULL COMMENT '串联单顺序',
+  `weapon_id` int(11) DEFAULT NULL COMMENT '武器id',
+  `weapon_attr` varchar(10000) DEFAULT NULL COMMENT '武器属性',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for `sz_user`
+-- ----------------------------
+DROP TABLE IF EXISTS `sz_user`;
+CREATE TABLE `sz_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `salt` varchar(255) DEFAULT NULL,
+  `create_time` varchar(255) DEFAULT NULL,
+  `locked` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sz_user
+-- ----------------------------
+INSERT INTO `sz_user` VALUES ('2', 'admin', '6e2ca1de0bccdafd2a75dc35740317a1', '5109f061b1b2002adecf8814a4680ae8', null, null);
 
 -- ----------------------------
 -- Table structure for `weapon`
@@ -28,5 +61,6 @@ CREATE TABLE `weapon` (
   `weapon_thumbnail` varchar(255) DEFAULT NULL COMMENT '武器缩略图地址',
   `weapon_model` varchar(255) DEFAULT NULL COMMENT '武器模型地址',
   `weapon_texture` varchar(255) DEFAULT NULL,
+  `weapon_gh_path` varchar(255) DEFAULT NULL COMMENT 'gh路径',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
